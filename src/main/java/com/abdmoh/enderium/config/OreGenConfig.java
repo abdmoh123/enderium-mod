e29f3be3cbb2c7d2f425f58df4910a9d9a3483fc
abdmoh123 <abdhawisa@gmail.com>
2020-03-15 19:46:02 +0000

    Cleaned up ore gen Added more ore gen config options

--- src/main/java/com/abdmoh/enderium/config/OreGenConfig.java
+++ src/main/java/com/abdmoh/enderium/config/OreGenConfig.java
@@ -4,27 +4,62 @@ import net.minecraftforge.common.ForgeConfigSpec;
 
 public class OreGenConfig {
     public static ForgeConfigSpec.IntValue enderium_chance;
+    public static ForgeConfigSpec.IntValue enderium_count;
+    public static ForgeConfigSpec.IntValue enderium_bottom;
+    public static ForgeConfigSpec.IntValue enderium_top;
+
     public static ForgeConfigSpec.IntValue netherite_chance;
+    public static ForgeConfigSpec.IntValue netherite_count;
+    public static ForgeConfigSpec.IntValue netherite_bottom;
+    public static ForgeConfigSpec.IntValue netherite_top;
+
     public static ForgeConfigSpec.BooleanValue generate_end;
     public static ForgeConfigSpec.BooleanValue generate_nether;
 
     public static void init(ForgeConfigSpec.Builder server, ForgeConfigSpec.Builder client) {
         server.comment("OreGen Config");
 
+        //enderium generation config
+        enderium_count = server
+                .comment("Maximum number of mystical crystals in a vein")
+                .defineInRange("oregen.enderium_count", 4, 1, 100);
+
         enderium_chance = server
                 .comment("Maximum number of mystical crystal veins that can spawn in one chunk")
-                .defineInRange("oregen.enderium_chance", 100, 1, 100000);
+                .defineInRange("oregen.enderium_chance", 2, 1, 100);
+
+        enderium_bottom = server
+                .comment("Lowest y-value mystical crystals can spawn at")
+                .defineInRange("oregen.enderium_bottom", 4, 0, 256);
+
+        enderium_top = server
+                .comment("Highest y-value mystical crystals can spawn at")
+                .defineInRange("oregen.enderium_top", 25, 0, 256);
+
+        //netherite generation config
+        netherite_count = server
+                .comment("Maximum number of ancient debris  in a vein")
+                .defineInRange("oregen.netherite_chance", 4, 1, 100);
 
         netherite_chance = server
                 .comment("Maximum number of ancient debris veins that can spawn in one chunk")
-                .defineInRange("oregen.netherite_chance", 100, 1, 100000);
+                .defineInRange("oregen.enderium_chance", 2, 1, 100);
 
+        netherite_bottom = server
+                .comment("Lowest y-value ancient debris can spawn at")
+                .defineInRange("oregen.enderium_bottom", 4, 0, 256);
+
+        netherite_top = server
+                .comment("Highest y-value ancient debris can spawn at")
+                .defineInRange("oregen.enderium_top", 20, 0, 256);
+
+        //general generation config
         generate_end = server
-                .comment("Decide if you want mystical crystals to spawn")
+                .comment("Decide if you want end generation")
                 .define("oregen.generate_end", true);
 
         generate_nether = server
-                .comment("Decide if you want ancient debris to spawn")
+                .comment("Decide if you want nether generation")
                 .define("oregen.generate_nether", true);
     }
 }
--- src/main/java/com/abdmoh/enderium/world/OreGen.java
+++ src/main/java/com/abdmoh/enderium/world/OreGen.java
@@ -22,15 +22,15 @@ public class OreGen {
             for (Biome biome: ForgeRegistries.BIOMES) {
                 biome.addFeature(Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(
                         FillerBlockType.create("END_STONE", "end_stone", new BlockMatcher(Blocks.END_STONE)),
-                        ModBlocks.MYSTICALCRYSTAL.getDefaultState(), OreGenConfig.enderium_chance.get()), Placement.COUNT_RANGE,
-                        new CountRangeConfig(2, 4, 0, 25)));
+                        ModBlocks.MYSTICALCRYSTAL.getDefaultState(), OreGenConfig.enderium_count.get()), Placement.COUNT_RANGE,
+                        new CountRangeConfig(OreGenConfig.enderium_chance.get(), OreGenConfig.enderium_bottom.get(), 0, OreGenConfig.enderium_top.get())));
             }
         }
         if (OreGenConfig.generate_nether.get()) {
             for (Biome biome: ForgeRegistries.BIOMES) {
                 biome.addFeature(Decoration.UNDERGROUND_ORES, Biome.createDecoratedFeature(Feature.ORE, new OreFeatureConfig(FillerBlockType.NETHERRACK,
-                        ModBlocks.ANCIENTDEBRIS.getDefaultState(), OreGenConfig.netherite_chance.get()), Placement.COUNT_RANGE,
-                        new CountRangeConfig(2, 4, 0, 20)));
+                        ModBlocks.ANCIENTDEBRIS.getDefaultState(), OreGenConfig.netherite_count.get()), Placement.COUNT_RANGE,
+                        new CountRangeConfig(OreGenConfig.netherite_chance.get(), OreGenConfig.netherite_bottom.get(), 0, OreGenConfig.netherite_top.get())));
             }
         }
     }
